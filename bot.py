c58827210a623951fae50faab8dc8579ebd4aedf
Жмышенко Абобус Альбертович <102914352+itskekoff@users.noreply.github.com>
2022-04-25 07:38:46 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 bot = discord.Bot() # для slash команд
 botname = config.BOT
 discordlink = config.DISCORD
-testingservers = [967316694160789589] # мой тестовый сервер, поменяй на свой, можно выделить несколько, если их несколько.
+testingservers = config.GUILDS # мой тестовый сервер, поменяй на свой, можно выделить несколько, если их несколько.
 
 
 def attack_server(ip):
@@ -23,25 +23,26 @@ async def on_ready():
     while True:
         await bot.change_presence(activity=discord.Game(name=discordlink))
         await asyncio.sleep(10.0)
-        await bot.change_presence(activity=discord.Game(name=config.PRESENCE1))
+        await bot.change_presence(activity=discord.Game(name="/attack - атака!"))
         await asyncio.sleep(10.0)
-        await bot.change_presence(activity=discord.Game(name=config.PRESENCE2))
+        await bot.change_presence(activity=discord.Game(name="/credits - инфа!"))
 
-@bot.slash_command(guild_ids = testingservers, name = config.CREDITS, description = config.CREDITS_DESC)
+
+@bot.slash_command(guild_ids = testingservers, name = "credits", description = "Информация о создателе!")
 @commands.cooldown(1, 60, commands.BucketType.user)
 async def credits(ctx):
     embed = discord.Embed(
         title='Информация о создателе!',
-        description=f'Надо проинформировать {ctx.author.mention}!',
+        description=f'Надо проинформаровать {ctx.author.mention}!',
         color=discord.Colour.red()
     )
-    embed.add_field(name='➦ Создатель', value=config.OWNER, inline=False)
-    embed.add_field(name='➦ Кодер', value=config.CODER, inline=False)
-    embed.add_field(name='➦ Создание', value=config.CREATED_WITH, inline=False)
-    embed.set_image(url=config.GIF)
+    embed.add_field(name='➦ Создатель', value ='<@731428949665513572>', inline=False)
+    embed.add_field(name='➦ Кодер', value ='<@967316503651315782>', inline=False)
+    embed.add_field(name='➦ Создание', value ='Создна на Python, с кофейком и с любовью<3', inline=False)
+    embed.set_image(url='https://i.gifer.com/origin/e9/e9329b236da049a682773a98868ba2c1.gif')
     await ctx.respond(embed=embed)
 
-@bot.slash_command(guild_ids = testingservers, name = config.RELOAD, description = config.RELOAD_DESC)
+@bot.slash_command(guild_ids = testingservers, name = "reload", description = "Перезагрузка!")
 @commands.has_permissions(administrator=True)
 async def reload(ctx):
     try:
@@ -50,7 +51,7 @@ async def reload(ctx):
     except:
         await ctx.respond("Бот умер, перезапустите пожалуйта!")
 
-@bot.slash_command(guild_ids = testingservers, name = config.CLEAR, description = config.CLEAR_DESC)
+@bot.slash_command(guild_ids = testingservers, name = "clear", description = "Очистить сообщения!")
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, limit: int):
         await ctx.channel.purge(limit=limit)
@@ -58,7 +59,7 @@ async def clear(ctx, limit: int):
         await ctx.respond(embed=embed1, delete_after=10)
         await ctx.message.delete()
 
-@bot.slash_command(guild_ids = testingservers, name = config.ATTACK, description = config.ATTACK_DESC)
+@bot.slash_command(guild_ids = testingservers, name = "attack", description = "Атака сервера!")
 @commands.cooldown(1, 60, commands.BucketType.user)
 async def attack(ctx, ip):
     def attack_start():
@@ -76,7 +77,7 @@ async def attack(ctx, ip):
     embed.set_footer(text="DDoS by " + botname)
     await ctx.respond(embed=embed)
 
-@bot.slash_command(guild_ids = testingservers, name = config.RESOLVE, description = config.RESOLVE_DESC)
+@bot.slash_command(guild_ids = testingservers, name = "resolve", description = "Узнать айпи сервера!")
 @commands.cooldown(1, 60, commands.BucketType.user)
 async def resolve(ctx, server):
     url = "https://api.mcsrvstat.us/2/" + server
