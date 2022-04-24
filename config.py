7dd735b916da88176525606ec03046c8a11a3889
Жмышенко Абобус Альбертович <102914352+itskekoff@users.noreply.github.com>
2022-04-25 03:00:13 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT = "HackerCom" # Название бота
-TOKEN = "OTY3NzUyMzE1MDc3NTI5NjYz.YmU3kw.t0gyuU7LQavpHYHBPQTEBifl1U0" # токен бота
+TOKEN = "токен" # токен бота
 DISCORD = "dsc.gg/hackercom" # ссылка на дс сервер
 
 # Названия команд
@@ -13,3 +13,17 @@ RESOLVE = "resolve" # узнать цифровой ip адрес
 
 PRESENCE1 = "/attack - атака!" # Статус бота (команда атаки)
 PRESENCE2 = "/credits - инфа!" # Статус бота (команда credits)
+ATTACK_GIF = "https://c.tenor.com/-SV9TjUGabMAAAAM/hacker-python.gif" # гифка которая будет отображатся при атаке
+# Описания команд
+
+CREDITS_DESC = "Информация о создателе!"
+RELOAD_DESC = "Перезагрузка!"
+CLEAR_DESC = "Очистить сообщения!"
+ATTACK_DESC = "Атака сервера!"
+RESOLVE_DESC = "Узнать айпи сервера!"
+
+# Информация о создателях
+OWNER = "<@731428949665513572>"
+CODER = "<@823635176575467550>"
+CREATED_WITH = "Создна на Python, с кофейком и с любовью<3"
+GIF = "https://i.gifer.com/origin/e9/e9329b236da049a682773a98868ba2c1.gif"
