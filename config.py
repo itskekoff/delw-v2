46e47dad269fc77b1df050c2000355e1d70f9eb1
Жмышенко Абобус Альбертович <102914352+itskekoff@users.noreply.github.com>
2022-04-25 07:39:04 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,7 @@
 BOT = "HackerCom" # Название бота
-TOKEN = "token" # токен бота
+TOKEN = "" # токен бота
 DISCORD = "dsc.gg/hackercom" # ссылка на дс сервер
-
-# Названия команд
-CREDITS = "credits" # инфа о боте
-ATTACK = "attack" # запуск ддоса
-CLEAR = "clear" # очистка канала
-RELOAD = "reload" # рестарт бота
-RESOLVE = "resolve" # узнать цифровой ip адрес
-
+GUILDS = [967316694160789589] # id серверов, где будут работать команды
 # Прочие мелочи
 
-PRESENCE1 = "/attack - атака!" # Статус бота (команда атаки)
-PRESENCE2 = "/credits - инфа!" # Статус бота (команда credits)
 ATTACK_GIF = "https://c.tenor.com/-SV9TjUGabMAAAAM/hacker-python.gif" # гифка которая будет отображатся при атаке
-# Описания команд
-
-CREDITS_DESC = "Информация о создателе!"
-RELOAD_DESC = "Перезагрузка!"
-CLEAR_DESC = "Очистить сообщения!"
-ATTACK_DESC = "Атака сервера!"
-RESOLVE_DESC = "Узнать айпи сервера!"
-
-# Информация о создателях
-OWNER = "<@731428949665513572>" # можно заменить на свой никнейм
-CODER = "<@823635176575467550>" # можно заменить на свой никнейм
-CREATED_WITH = "Создна на Python, с кофейком и с любовью<3"
-GIF = "https://i.gifer.com/origin/e9/e9329b236da049a682773a98868ba2c1.gif"
