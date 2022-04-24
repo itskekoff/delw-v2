032d1048a217400d0b89de3d8053983d4985b0e7
Жмышенко Абобус Альбертович <102914352+itskekoff@users.noreply.github.com>
2022-04-25 02:54:09 +1000

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT = "HackerCom" # Название бота
+TOKEN = "OTY3NzUyMzE1MDc3NTI5NjYz.YmU3kw.t0gyuU7LQavpHYHBPQTEBifl1U0" # токен бота
+DISCORD = "dsc.gg/hackercom" # ссылка на дс сервер
+
+# Названия команд
+CREDITS = "credits" # инфа о боте
+ATTACK = "attack" # запуск ддоса
+CLEAR = "clear" # очистка канала
+RELOAD = "reload" # рестарт бота
+RESOLVE = "resolve" # узнать цифровой ip адрес
+
+# Прочие мелочи
+
+PRESENCE1 = "/attack - атака!" # Статус бота (команда атаки)
+PRESENCE2 = "/credits - инфа!" # Статус бота (команда credits)
